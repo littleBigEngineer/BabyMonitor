6d0431287339b42ff7413e007ad5182f829ed3fb
Robert Crowley <robert.crowley1995@gmail.com>
2018-01-19 18:51:52 +0000

    Added Firebase connectivity

--- Mobile Application/app/src/main/java/neo/baba/neonatalmonitoring/Login.java
+++ Mobile Application/app/src/main/java/neo/baba/neonatalmonitoring/Login.java
@@ -2,7 +2,18 @@ package neo.baba.neonatalmonitoring;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Login extends AppCompatActivity {
 
@@ -11,5 +22,37 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login_activity);
+
+
+    }
+
+    public void event(View view){
+        EditText edit = (EditText)findViewById(R.id.username);
+        System.out.println(edit.getText().toString());
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("documents");
+
+
+        //myRef.setValue(edit.getText().toString());
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+                String value = dataSnapshot.;
+
+                System.out.println("Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //System.out.println("Failed to read value.", error.toException());
+            }
+        });
+
     }
 }
