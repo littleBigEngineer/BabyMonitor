cc93971517b59e24d4d16c96629980f5069cd27d
Robert Crowley <robert.crowley1995@gmail.com>
2018-01-28 22:31:10 +0000

    Updated Dashboard of mobile app

--- Mobile Application/app/src/main/java/neo/baba/neonatalmonitoring/Dashboard.java
+++ Mobile Application/app/src/main/java/neo/baba/neonatalmonitoring/Dashboard.java
@@ -1,20 +1,20 @@
 package neo.baba.neonatalmonitoring;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,6 +25,9 @@ public class Dashboard extends AppCompatActivity {
 
     FirebaseDatabase database;
     final int ROOM_TEMP = 20;
+    int reading;
+    FirebaseAuth mAuth;
+    boolean type = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,33 +35,38 @@ public class Dashboard extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dashboard);
         database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         final DatabaseReference temperature = database.getReference("Temperature");
+        final ImageView cold = findViewById(R.id.cold);
+        final ImageView temperate = findViewById(R.id.temperate);
+        final ImageView hot = findViewById(R.id.hot);
 
         temperature.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String currentTemp = dataSnapshot.child("Current Temp").getValue().toString();
                 float current = Math.round(Float.parseFloat(currentTemp));
-                String avgTemp = dataSnapshot.child("Average Temp").getValue().toString();
-                float average = Math.round(Float.parseFloat(avgTemp));
-
-                TextView curr = findViewById(R.id.currTemp);
-                curr.setText((int)current+"\u00b0");
-                TextView avg = findViewById(R.id.avgTemp);
-                avg.setText("Average:" + (int)average+"\u00b0");
-                ConstraintLayout temp = findViewById(R.id.temperature);
+                final TextView curr = findViewById(R.id.currTemp);
+                reading = (int)current;
+                curr.setText((int)current+"\u00b0C");
 
                 if(current > ROOM_TEMP+5){
                     createNotification("Your baby needs you!", "The temperature in the room is too hot!");
-                    temp.setBackgroundColor(Color.parseColor("#e50000"));
+                    hot.setBackgroundResource(R.drawable.red_circ);
+                    cold.setBackgroundResource(R.drawable.empty_circ);
+                    temperate.setBackgroundResource(R.drawable.empty_circ);
                 }
                 else if(current < ROOM_TEMP-5){
                     createNotification("Your baby needs you!", "The temperature in the room is too cold!");
-                    temp.setBackgroundColor(Color.parseColor("#4C9CEC"));
+                    cold.setBackgroundResource(R.drawable.blue_circ);
+                    hot.setBackgroundResource(R.drawable.empty_circ);
+                    temperate.setBackgroundResource(R.drawable.empty_circ);
                 }
                 else{
-                    temp.setBackgroundColor(Color.parseColor("#F39c12"));
+                    cold.setBackgroundResource(R.drawable.empty_circ);
+                    hot.setBackgroundResource(R.drawable.empty_circ);
+                    temperate.setBackgroundResource(R.drawable.green_circ);
                 }
             }
 
@@ -67,6 +75,52 @@ public class Dashboard extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void tempSwitch(View view){
+        final ConstraintLayout tempLayout = findViewById(R.id.temperature);
+        final Switch temp_switch = findViewById(R.id.temp_switch);
+        Boolean switch_status = temp_switch.isChecked();
+
+        if(!switch_status) {
+            tempLayout.setEnabled(false);
+
+            switchToast(0,"Temperature");
+        }
+        else {
+            tempLayout.setEnabled(true);
+            switchToast(1, "Temperature");
+        }
+    }
+
+    public void celsToFar(View view){
+        final TextView curr = findViewById(R.id.currTemp);
+        if(type) {
+            double far = (reading * 1.8) + 32;
+            far = Math.round(far);
+            curr.setText((int) far + "\u00b0F");
+            type = false;
+        }
+        else {
+            curr.setText(reading + "\u00b0C");
+            type = true;
+        }
+    }
+
+    public void switchToast(int type, String module){
+        String status = "";
+        if(type == 0)
+            status = "disabled";
+        else
+            status = "enabled";
+
+        Toast.makeText(Dashboard.this, module + " Module " + status, Toast.LENGTH_SHORT).show();
+    }
+    public void logout(View view){
+        mAuth.signOut();
+        Intent logout = new Intent(Dashboard.this, Login.class);
+        Dashboard.this.startActivity(logout);
+        Toast.makeText(Dashboard.this, "Signed Out", Toast.LENGTH_SHORT).show();
     }
 
     public void createNotification(String title, String content){
--- Mobile Application/app/src/main/java/neo/baba/neonatalmonitoring/Loading.java
+++ Mobile Application/app/src/main/java/neo/baba/neonatalmonitoring/Loading.java
@@ -5,31 +5,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import neo.baba.neonatalmonitoring.neo.baba.neonatalmonitoring.model.Account;
-
 public class Loading extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.loading_activity);
-
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -39,7 +22,6 @@ public class Loading extends AppCompatActivity {
             }
         }, 2000);
     }
-
     @Override
     public void onBackPressed() {
         Toast.makeText(Loading.this, "Sorry, You can't do that right now", Toast.LENGTH_SHORT).show();
